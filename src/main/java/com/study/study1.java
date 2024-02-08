5965a19c1559003fe59561b05fd630141922db0e
Kimjiho <rlawlgh1204@gmail.com>
2024-02-08 18:18:44 +0900

    commit

---
+++ src/main/java/com/study/study1.java
@@ -0,0 +1,95 @@
+package com.study;
+
+import java.util.Scanner;
+
+public class study1 {
+
+    public static void main(String[] args) {
+
+//        for (int i = 2; i <= 19; i++) {
+//            for (int j = 1; j <= 19; j++) {
+//
+//                System.out.println(i + "*" + j + "=" + (i * j));
+//            }
+//            System.out.println();
+
+//        for (int i=1; i<=5; i++) {
+//            System.out.println("***********");
+
+//        for (int i = 1; i <= 20; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        for (int i=1; i<=20; i++) {
+//            for (int j=1; j<i; j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        int i = 5;
+//        while (i !=0) {
+//            i--;
+//            System.out.println(i + " - i can do it");
+//        int sum = 0;
+//        int i = 0;
+//        while (sum <=100 ){
+//            System.out.println();
+//        int[] iarr = new int[5];
+//
+//        System.out.println(iarr[0]);
+//        System.out.println(iarr[1]);
+//        System.out.println(iarr[2]);
+//        System.out.println(iarr[3]);
+//        System.out.println(iarr[4]);
+//
+//        for (int i = 0; i < iarr.length; i++) {
+//            System.out.println("iarr["+ i +"]의 값 : " + iarr[1]);
+
+//        int[][] iarr = new int[3][4];
+//        int value = 1;
+//        for (int i = 0; i < iarr.length; i++) {
+//            for (int j = 0; j < iarr[i].length; j++) {
+//                iarr[i][j] = value++;
+//                System.out.print(iarr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//        for (int i = 0; i < iarr.length; i++){
+//
+//            for (int j = 0; j < iarr[i].length; i++) {
+//                System.out.println(iarr[i][j] + "");
+//            }
+//            System.out.println();
+
+        int[] origin = {1, 2, 3, 4, 5};
+        int[] deepCopy = {1, 2, 3, 4, 5};
+
+//        System.out.println("origin hashCode : " + origin.hashCode());
+//        System.out.println("copy hashCode : " + copy.hashCode());
+//        System.out.println();
+
+//        for (int i = 0; i < origin.length; i++){
+//            for (int j = 0; j < origin[i]; j++) {
+//
+//            }
+//        }
+//        System.out.println();
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
